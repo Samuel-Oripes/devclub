2ccebc24602e1e12db8347dc39f53ca7c1ee9ef3
Samuel-Oripes <moreiraoripessamuelvictor@gmail.com>
2024-12-02 16:19:26 -0300

    tornando o código mais técnico

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,10 @@
 
 import { Router } from "express";
 
-const routes = new Router()
+const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.status(200).json({ message: 'Hello World!'})
-})
+routes.get("/", (req, res) => {
+	return res.status(200).json({ message: "Hello World!" });
+});
 
-export default routes
+export default routes;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 //expõem a aplicação
 
-import app from "./app.js"
+import app from "./app.js";
 
-app.listen(3001, () => console.log('O server está rodando na porta 3001'))
+app.listen(3001, () => console.log("O server está rodando na porta 3001"));
