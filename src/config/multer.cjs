7c232911c56980982b2769ab86a088af39a135c9
Samuel-Oripes <moreiraoripessamuelvictor@gmail.com>
2024-12-06 17:45:14 -0300

    Back-end

---
+++ src/config/multer.cjs
@@ -0,0 +1,12 @@
+const multer = require("multer");
+const { v4 } = require("uuid");
+
+const { extname, resolve } = require("node:path");
+
+module.exports = {
+	storage: multer.diskStorage({
+		destination: resolve(__dirname, "..", "..", "uploads"),
+		filename: (req, file, callback) =>
+			callback(null, v4() + extname(file.originalname)),
+	}),
+};
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
 //rotas
 
 import { Router } from "express";
+import multer from "multer";
+import multerConfig from "./config/multer.cjs";
+
 import SessionController from "./app/controllers/SessionController.js";
 import UserController from "./app/controllers/UserController.js";
+import ProductController from "./app/controllers/productController.js";
 
 const routes = new Router();
 
+const upload = multer(multerConfig);
+
 routes.post("/users", UserController.store);
 routes.post("/session", SessionController.store);
+routes.post("/products", upload.single('file'), ProductController.store);
 
 export default routes;
